4a61c8b3ca159c424df8c29f88a3b37f8f287b03
vinieFortes <vinie4apps@gmail.com>
2022-04-05 12:02:26 -0300

    Novo Commit

--- script/send_git.sh
+++ script/send_git.sh
@@ -5,9 +5,10 @@ rm -r docs
 mkdir "docs"
 mv  -v dist/* docs/
 
-git add .
+git add -A
 git commit -m "Novo Commit"
-git push origin branch-name
+git remote add origin "https://github.com/VinieFortes/windowsXP_Vue.git"
+git push "https://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@github.com/vinieFortes/windowsXP_Vue.git"
 
 echo "==================================================================================="
 echo "                              C O N C L U Í D O "
